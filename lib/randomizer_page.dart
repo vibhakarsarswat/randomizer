21120976218235ab41e12f823cda5d2bd58f12e7
vibhakarsarswat <vibhakar.sarswat@gmail.com>
2022-01-26 00:29:40 +0100

    Random (class): Generated a RandomNumber (using FAB) using Min and Max and displayed it.

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -15,12 +17,30 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+  final randomGenerator = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? 'Generate a number',
+          style: const TextStyle(fontSize: 42),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min + randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
