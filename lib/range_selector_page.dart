1900fef4947c1ab376b49974c81d753b8e2dda63
vibhakarsarswat <vibhakar.sarswat@gmail.com>
2022-01-26 01:51:01 +0100

    Hooks (RangeSelectorPage): Added Hooks plugin to replace Stateful class with 'HookWidget' i.e. 'RangeSelectorPage' now extends 'HookWidget' that extends a 'StatelessWidget'

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:randomizer/randomizer_page.dart';
 import 'package:randomizer/range_selector_form.dart';
 
-class RangeSelectorPage extends StatefulWidget {
-  const RangeSelectorPage({Key? key}) : super(key: key);
+class RangeSelectorPage extends HookWidget {
+  RangeSelectorPage({Key? key}) : super(key: key);
 
-  @override
-  _RangeSelectorPageState createState() => _RangeSelectorPageState();
-}
-
-class _RangeSelectorPageState extends State<RangeSelectorPage> {
   // Created a GlobalKey of 'type = <FormState>' to provide a Widget and its state throughout the app.
   final formKey = GlobalKey<FormState>();
-  int _minValue = 0;
-  int _maxValue = 0;
 
   @override
   Widget build(BuildContext context) {
+    final minNum = useState<int>(0);
+    final maxNum = useState<int>(0);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
       body: RangeSelectorForm(
         formKey: formKey,
-        minValueSetter: (value) => _minValue = value, //Callback
-        maxValueSetter: (value) => _maxValue = value, //Callback
+        minValueSetter: (value) => minNum.value = value, //Callback
+        maxValueSetter: (value) => maxNum.value = value, //Callback
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
@@ -35,8 +32,8 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
             Navigator.of(context).push(
               MaterialPageRoute(
                 builder: (context) => RandomizerPage(
-                  min: _minValue,
-                  max: _maxValue,
+                  min: minNum.value,
+                  max: minNum.value,
                 ),
               ),
             );
