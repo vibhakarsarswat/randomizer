b7c4cfa41632d1d0903a714593d8d422e38c586d
vibhakarsarswat <vibhakar.sarswat@gmail.com>
2022-01-28 00:23:32 +0100

    Provider: Added 'Provider' package. Wrapped 'MaterialApp' inside 'ChangeNotifierProvider' that provides 'Context' of 'RandomizerChangeNotifier'. Removed 'flutter_hooks' dependency.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:randomizer/randomizer_change_notifier.dart';
 import 'package:randomizer/range_selector_page.dart';
 
 void main() {
@@ -11,10 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Randomizer',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: RangeSelectorPage(),
+    return ChangeNotifierProvider(
+      create: (context) => RandomizerChangeNotifier(),
+      child: MaterialApp(
+        title: 'Randomizer',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: RangeSelectorPage(),
+      ),
     );
   }
 }
